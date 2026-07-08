eff203b05bdcce0cc3b5479c8425df74d271ce5d
mguarinoi <mguarinoi@unjbg.edu.pe>
2026-07-08 10:17:46 -0500

    Creacion del bucle para crear la matriz bidimensional.

--- matriz_transpuesta.cpp
+++ matriz_transpuesta.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 using namespace std;
 int main(){
+	int M[100][100];
+	int n, m;
+	cin >>n>>m;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>M[i][j];
+		}
+	}
 	
 	
 	
