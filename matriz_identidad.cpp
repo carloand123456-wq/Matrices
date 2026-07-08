24b030795e18bb967b5104015c76882d06264acf
mguarinoi <mguarinoi@unjbg.edu.pe>
2026-07-08 09:51:57 -0500

    No hacer cambios, terminado.

---
+++ matriz_identidad.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int M[100][100];
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(i == j){
+				cout<<"1";
+			}
+			else{
+				cout<<"0";
+			}
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
