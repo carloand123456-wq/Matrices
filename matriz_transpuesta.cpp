038e7427718142373ede1c3e3b40ba763b3d9d3a
carloand123456-wq <carloand123456@gmail.com>
2026-07-08 19:24:13 -0500

    No hacer cambios, terminado.

--- matriz_transpuesta.cpp
+++ matriz_transpuesta.cpp
@@ -1,21 +1,27 @@
-//matriz_transpuesta.cpp
+// matriz_transpuesta.cpp
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 	int M[100][100];
 	int n, m;
-	cin >>n>>m;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin>>M[i][j];
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> M[i][j];
 		}
 	}
-	
-	for(int i=0;i<m;i++){
-		for(int j=0;j<n;j++){
-			
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << M[j][i] << " ";
 		}
+		cout << endl;
 	}
-	
+
 	return 0;
 }
